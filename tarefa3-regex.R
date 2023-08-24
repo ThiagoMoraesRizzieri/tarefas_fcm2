937f448dba5f1ccfb783e8d2736243e5408cfef2
Thiago Moraes Rizzieri <83147343+ThiagoMoraesRizzieri@users.noreply.github.com>
2023-08-24 14:37:33 -0300

    arrumei comentário

--- tarefa3-regex.R
+++ tarefa3-regex.R
@@ -9,8 +9,8 @@ x <- pdf[grepl("CEP:", pdf)]
 
 # Priemiramente, iremos substituir tudo aquilo que começa com qualquer caracter até o caracter "CEP: " que será excluido também.
 
-# ^(.*) indica todos os caracteres presentes desde o começo.
-# ^(.*?)(CEP: ) indica para pegar todas os caracteres "CEP: ", sendo que pode haver mais caracteres antes dele ou não.
+# (.*) indica todos os caracteres.
+# (.*?)(CEP: ) indica para pegar todas os caracteres "CEP: ", sendo que pode haver mais caracteres antes dele ou não.
 # Isso será necessário para lidarmos com os caracteres presentes antes de "CEP: " na segunda linha.
 x <- gsub("(.*?)(CEP: )", "", x)
 
