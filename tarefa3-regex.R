ce80d8a958664d16cf1b2305d3543f2d365a9c25
Thiago Moraes Rizzieri <83147343+ThiagoMoraesRizzieri@users.noreply.github.com>
2023-09-07 18:04:41 -0300

    tarefa 4

--- tarefa3-regex.R
+++ tarefa3-regex.R
@@ -7,14 +7,14 @@ pdf
 # Extrando do pdf as linhas que possuem o conjunto de caracteres "CEP:"
 x <- pdf[grepl("CEP:", pdf)]
 
-# Priemiramente, iremos substituir tudo aquilo que comeÃ§a com qualquer caracter atÃ© o caracter "CEP: " que serÃ¡ excluido tambÃ©m.
+# Priemiramente, iremos substituir tudo aquilo que começa com qualquer caracter até o caracter "CEP: " que será excluido também.
 
-# (.*) indica todos os caracteres.
-# (.*?)(CEP: ) indica para pegar todas os caracteres "CEP: ", sendo que pode haver mais caracteres antes dele ou nÃ£o.
-# Isso serÃ¡ necessÃ¡rio para lidarmos com os caracteres presentes antes de "CEP: " na segunda linha.
+# ^(.*) indica todos os caracteres presentes desde o começo.
+# ^(.*?)(CEP: ) indica para pegar todas os caracteres "CEP: ", sendo que pode haver mais caracteres antes dele ou não.
+# Isso será necessário para lidarmos com os caracteres presentes antes de "CEP: " na segunda linha.
 x <- gsub("(.*?)(CEP: )", "", x)
 
-# Por fim, retiramos os caracteres "-" e "." para deixar apenas nÃºmeros no CEP.
+# Por fim, retiramos os caracteres "-" e "." para deixar apenas números no CEP.
 x <- gsub("-|\\.", "", x)
 
 
